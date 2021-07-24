1fa2c26aab98be7a043126f5bb6f9a13bda4e66a
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-07-24 13:20:01 +0500

    create GraphQL Server

---
+++ schema.js
@@ -0,0 +1,65 @@
+import axios from "axios"
+import {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
+} from "graphql"
+
+// Launch Types
+
+const LaunchType = new GraphQLObjectType({
+  name: "Lauch",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+  }),
+})
+
+// Rocket Type
+
+const RocketType = new GraphQLObjectType({
+  name: "Rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString },
+  }),
+})
+
+// Root Query
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      async resolve(parent, args) {
+        const { data } = await axios.get(
+          "https://api.spacexdata.com/v3/launches"
+        )
+        return data
+      },
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt },
+      },
+      async resolve(parent, args) {
+        const { data } = await axios.get(
+          `https://api.spacexdata.com/v3/launches/${args.flight_number}`
+        )
+        return data
+      },
+    },
+  },
+})
+
+export default new GraphQLSchema({ query: RootQuery })
