6ed09142e9e1fae5b6866a85a355ad8fddf66228
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-07-25 22:59:20 +0500

    add comments

--- hello world/demo.js
+++ hello world/demo.js
@@ -1,5 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server")
 
+// When configuring graphQL we will first create Schema using SDL
+// Then we will add fields to the query type.
+
+// if we want the response to go in a certain format then the schema should know about it becuase
+
 const typeDefs = gql`
   type User {
     email: String
@@ -12,8 +17,11 @@ const typeDefs = gql`
   }
 `
 
+// Resolver fetch the data you want from any source you want. You can use async functions any kind of data.
+
 const resolvers = {
   Query: {
+    // the query and query field name in this case it is me it should match the query.
     me() {
       return {
         email: "ali@gmail.com",
